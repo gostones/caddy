ecc47758774ad136e6312b2a370fb99156c77472
Qiang Li <liqiang@gmail.com>
2019-03-02 15:40:54 -0800

    git plugin

--- caddy/caddymain/run.go
+++ caddy/caddymain/run.go
@@ -39,6 +39,7 @@ import (
 
 	_ "github.com/mholt/caddy/caddyhttp" // plug in the HTTP server type
 	// This is where other plugins get plugged in (imported)
+	_ "github.com/abiosoft/caddy-git"
 )
 
 func init() {
